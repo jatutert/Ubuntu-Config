cc7c6a919921949c0e6f9b514b7da0584f1ee2cd
John Tutert <141269577+jatutert@users.noreply.github.com>
2023-09-21 18:43:03 +0200

    Add files via upload

---
+++ docker-pull-images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker pull hello-world 		
+docker pull prakhar1989/static-site  
+docker pull alpine 			
+docker pull registry  		
+docker pull odoo:latest		
+docker pull nextcloud			
+docker pull mariadb:10.6		
+docker pull postgres:latest	
+docker pull minio/minio
