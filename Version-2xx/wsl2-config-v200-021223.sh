82155306667d64c11b327fe6ffe444893a61f6ce
jatutert <jatutert@gmail.com>
2023-12-04 19:56:08 +0100

    update

--- Version-2xx/wsl2-config-v200-021223.sh
+++ Version-2xx/wsl2-config-v200-021223.sh
@@ -2,7 +2,7 @@
 #
 #
 # Configuratiescript Ubuntu voor bijvoorbeeld Vagrant en WSL2 
-# Versie: 2.0.0 DEVELOP d.d. 30-11-2023
+# Versie: 2.0.0 DEVELOP d.d. 02-12-2023
 # Auteur: John Tutert
 #
 # ######################
@@ -30,6 +30,8 @@
 # flask demo en minikube(2.0.0.241123 DEVELOP)
 # meerdere talen flash demo (2.0.0.301123 DEVELOP) 
 # ubuntu update repo als functie (2.0.0.301123 DEVELOP)
+# hosts bestand aanpassen (2.0.0.02122023 DEVELOP)
+#
 #
 #
 # Check of script wordt uitgevoerd als SUDO 
@@ -587,11 +589,77 @@ while true; do
             chown -f -R $SUDO_USER /home/$SUDO_USER/playbooks
             curl -s -o /home/$SUDO_USER/playbooks/ansible_demo_playbook.yml https://raw.githubusercontent.com/jatutert/Ansible/main/PlayBooks/Ubuntu-Linux/ansible_demo_playbook.yml
             #
-            # NOG TOEVOEGEN
+            # Aanpassen etc hosts bestand binnen ubuntu linux 
             #
-            # hosts bestand goed zetten 
-            # echo >> /etc/hosts
-            ;;
+            # Haal de hostname op
+            hostname=$(hostname)
+            #
+            # Controleer of de hostname gelijk is aan "ulx-s-mst-001"
+            if [ "$hostname" == "ulx-s-mst-001" ]; then
+                # Haal het IP-adres van eth1 op
+                eth1_ip=$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+                # Voeg 1 toe aan het IP-adres van eth1
+                IFS=. read -r a b c d <<< "$eth1_ip"
+                eth1_plus1_ip="$a.$b.$c.$((d+1))"
+                # Sla de IP-adressen op in afzonderlijke variabelen
+                eth1_ip_var="eth1_ip=$eth1_ip"
+                eth1_plus1_ip_var="eth1_plus1_ip=$eth1_plus1_ip"
+                # Aanpassen hosts bestand 
+                if grep -q "$eth1_ip" /etc/hosts; then
+                   echo "$master_hostname already exists in /etc/hosts"
+                else
+                   # Add the hostname and IP address to /etc/hosts
+                   echo "$eth1_ip_var" | sudo tee -a /etc/hosts > /dev/null
+                   echo "$eth1_plus1_ip_var" | sudo tee -a /etc/hosts > /dev/null
+                   echo "Hostname $master_hostname added to /etc/hosts"
+                fi
+            fi
+
+
+if [ "$hostname" == "ulx-s-slv-001" ]; then
+    # Haal het IP-adres van eth1 op
+    eth1_ip=$(ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+
+    # Voeg 1 toe aan het IP-adres van eth1
+    IFS=. read -r a b c d <<< "$eth1_ip"
+    eth1_plus1_ip="$a.$b.$c.$((d-1))"
+
+    # Sla de IP-adressen op in afzonderlijke variabelen
+    eth1_ip_var="eth1_ip=$eth1_ip"
+    eth1_min1_ip_var="eth1_plus1_ip=$eth1_plus1_ip"
+
+    # Print de variabelen
+    echo "$eth1_ip_var" | sudo tee -a /etc/hosts > /dev/null
+    echo "$eth1_min1_ip_var" | sudo tee -a /etc/hosts > /dev/null
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ;;
         9)
             echo "U heeft gekozen om het menu te verlaten."
             exit 0
