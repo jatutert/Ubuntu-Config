7df18ee32dc7adc8ea4ac7018c2f9f131bd4a7cd
John Tutert <jatutert@gmail.com>
2024-07-11 22:20:18 +0200

    Update Latest

--- Aanmaken-latest.cmd
+++ Aanmaken-latest.cmd
@@ -1,6 +1,6 @@
 ::
 :: Script voor het aanmaken van latest bestanden op basis van nieuwste bestand in directory
-:: 24 november 2024
+:: 11 juli 2024
 :: John Tutert 
 :: 
 
@@ -9,7 +9,7 @@
 :: Aanmaken LATEST voor WSL2 Config (versie 1)
 
 :: Stel de bron- en doelmap in
-set source=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config\Version-1xx
+set source=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config\Version-1xx-WSL2-No-Usermenu
 set destination=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config
 
 :: Zoek het nieuwste bestand in de bronmap
@@ -17,12 +17,12 @@ FOR /F "delims=" %%I IN ('DIR "%source%" /B /A:-D /O:-D') DO SET NewestFile=%%I 
 :Continue
 
 :: Maak een kopie van het nieuwste bestand met de oude naam en "latest" als toevoeging
-COPY "%source%\%NewestFile%" "%destination%\wsl2-config-latest.sh"
+COPY "%source%\%NewestFile%" "%destination%\ubuntu-config-V1-latest.sh"
 
 :: Aanmaken latest voor Ubuntu Config (versie 2)
 
 :: Stel de bron- en doelmap in
-set source=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config\Version-2xx
+set source=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config\Version-2xx-Usermenu-Edition
 set destination=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config
 
 :: Zoek het nieuwste bestand in de bronmap
@@ -30,5 +30,17 @@ FOR /F "delims=" %%I IN ('DIR "%source%" /B /A:-D /O:-D') DO SET NewestFile=%%I 
 :Continue
 
 :: Maak een kopie van het nieuwste bestand met de oude naam en "latest" als toevoeging
-COPY "%source%\%NewestFile%" "%destination%\ubuntu-config-latest.sh"
+COPY "%source%\%NewestFile%" "%destination%\ubuntu-config-V2-latest.sh"
 
+:: Aanmaken latest voor Ubuntu Config (versie 3)
+
+:: Stel de bron- en doelmap in
+set source=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config\Version-3xx-Distro-Independent
+set destination=D:\OneDrive\OneDrive - Saxion\Repository-Playground\Development\GitHub-JATUTERT-Repositories\Ubuntu-Config
+
+:: Zoek het nieuwste bestand in de bronmap
+FOR /F "delims=" %%I IN ('DIR "%source%" /B /A:-D /O:-D') DO SET NewestFile=%%I & GOTO Continue
+:Continue
+
+:: Maak een kopie van het nieuwste bestand met de oude naam en "latest" als toevoeging
+COPY "%source%\%NewestFile%" "%destination%\ubuntu-config-V3-latest.sh"
